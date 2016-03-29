8f3403644b791a72bdae5818c6262eac68e66433
guoyu19961004 <u201413384@hust.edu.cn>
2016-03-29 20:21:43 +0800

    愚人节

---
+++ javascripts/create.js
@@ -0,0 +1,49 @@
+var name,
+	href,
+	sex,
+	hearts = document.getElementsByClassName("heart"),
+	male = ['吴彦祖', '张震', '王凯', '吴磊', '吴亦凡', '鹿晗', '张艺兴', '王俊凯', '易烊千玺', '权志龙', '黄子韬', '胡歌', '宋仲基'],
+	female = ['苍老师', '柳岩', '罗玉凤', '林志玲', '波多野结衣', '容嬷嬷','小泽玛利亚','范冰冰','刘亦菲','汤唯','贾玲','杨幂'];
+//随机一个从min~max的整数
+function randomNumber(min, max) {
+	var cha = max - min;
+	var randomNumber = min + (Math.floor(Math.random() * cha));
+	return randomNumber;
+}
+//根据性别随机男星/女星姓名
+function randomName(sex, min, max) {
+	if (sex === "male") {
+		name = female[randomNumber(min, max)];
+	} else {
+		name = male[randomNumber(min, max)];
+	}
+}
+
+function showMaleHeart() {
+	sex = "male";
+	hearts[0].style.opacity = "1";
+	hearts[1].style.opacity = "0";
+}
+
+function showFemaleHeart() {
+	sex = "female";
+	hearts[1].style.opacity = "1";
+	hearts[0].style.opacity = "0";
+}
+
+function checked() {
+	if (sex == "male") {
+		randomName("male",0,female.length-1);
+		href = encodeURI("show.html?sex=" + sex + "&name=" + name + "&jump=ok");
+		window.location.href = href;
+	} else if (sex == "female") {
+		randomName("female",0,male.length-1);
+		href = encodeURI("show.html?sex=" + sex + "&name=" + name + "&jump=ok");
+		window.location.href = href;
+	} else {
+		alert("请选择性别！");
+	}
+}
+document.getElementsByTagName('label')[0].addEventListener("touchend", showMaleHeart, false);
+document.getElementsByTagName('label')[1].addEventListener("touchend", showFemaleHeart, false);
+document.getElementsByTagName('button')[0].addEventListener("touchend", checked, false);
---
+++ javascripts/show.js
@@ -0,0 +1,128 @@
+var weddingNext = document.getElementById("next"),
+	wedding = document.getElementById("wedding"),
+	share = document.getElementById("share"),
+	shareBg = document.getElementById('share-bg'),
+	create = document.getElementById("create"),
+	content = document.getElementById('content'),
+	weddingPic = document.getElementById('wedding-pic-wrap'),
+	showPic = document.getElementById('show-pic'),
+	close = document.getElementById('close'),
+	startY, removeY;
+
+//地址栏正则表达式
+function getQueryString(name) {
+	var href = decodeURI(window.location.search);
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = href.substr(1).match(reg);
+	if (r !== null) return unescape(r[2]);
+	return null;
+}
+
+//更改相关显示
+function judgeSex() {
+	var sex = getQueryString("sex");
+	if (sex && sex.toString().length > 1) {
+		if (sex === "male") {
+			document.getElementById("it1").innerHTML = "她";
+			document.getElementById("it2").innerHTML = "她";
+		}
+	}
+}
+
+function judgeName() {
+	var name = decodeURI(getQueryString("name"));
+	if (name && name.toString().length > 1) {
+		document.getElementById("name").innerHTML = name;
+	}
+}
+//判断用户从哪里进入,并完成转换
+function judgeUser() {
+	if (getQueryString("jump")) {
+		share.style.display = "block";
+		create.style.display = "none";
+	}
+	judgeSex();
+	judgeName();
+}
+
+function hideWedding() {
+	wedding.style.cssText = "-moz-transform: translateY(-100%);-ms-transform: translateY(-100%);-webkit-transform: translateY(-100%);transform: translateY(-100%);";
+	content.style.cssText = "-moz-transform: translateY(0);-ms-transform: translateY(0);-webkit-transform: translateY(0);transform: translateY(0);";
+	document.getElementsByClassName("text")[0].style.animation = "showText 1s ease-in-out 0.3s";
+	document.getElementsByClassName("text")[0].style.WebkitAnimation = "showText 1s ease-in-out 0.3s";
+	setTimeout(function () {
+		content.addEventListener("touchend", touchEnd, false);
+	},1300);
+	wedding.removeEventListener("touchend", touchEnd, false);
+}
+
+function showWedding() {
+	content.style.cssText = "-moz-transform: translateY(100%);-ms-transform: translateY(100%);-webkit-transform: translateY(100%);transform: translateY(100%);";
+	wedding.style.cssText = "-moz-transform: translateY(0);-ms-transform: translateY(0);-webkit-transform: translateY(0);transform: translateY(0);";
+	setTimeout(function () {
+		wedding.addEventListener("touchend", touchEnd, false);
+	},500);
+	content.addEventListener("touchend", touchEnd, false);
+}
+
+function showWeddingPic() {
+	weddingPic.style.display = "block";
+}
+
+function hideWeddingPic() {
+	weddingPic.style.display = "none";
+}
+
+function showShare() {
+	shareBg.style.display = "block";
+	shareBg.addEventListener("touchstart", hideShare, false);
+	share.removeEventListener("touchstart", showShare, false);
+}
+
+function hideShare() {
+	shareBg.style.display = "none";
+	share.addEventListener("touchstart", showShare, false);
+	shareBg.removeEventListener("touchstart", hideShare, false);
+}
+function jumpCreate() {
+	window.location.href = "create.html";
+}
+
+function touchStart(event) {
+	event.preventDefault();
+	if (!event.touches.length) return;
+	var touch = event.touches[0];
+	startY = touch.pageY;
+}
+
+function touchMove(event) {
+	event.preventDefault();
+	if (!event.touches.length) return;
+	var touch = event.touches[0];
+	removeY = touch.pageY - startY;
+}
+
+function touchEnd(event) {
+	if (removeY > 0) {
+		showWedding();
+	} else if (removeY < 0) {
+		hideWedding();
+	}
+	else return;
+}
+window.onload = function() {
+	judgeUser();
+	window.history.replaceState(null, null, "?sex=" + getQueryString("sex") + "&name=" + getQueryString("name"));
+};
+share.addEventListener("touchend", showShare, false);
+create.addEventListener("touchend", jumpCreate, false);
+weddingNext.addEventListener("touchend", hideWedding, false);
+showPic.addEventListener("touchstart", showWeddingPic, false);
+showPic.addEventListener("touchend", hideWeddingPic, false);
+wedding.addEventListener("touchstart", touchStart, false);
+wedding.addEventListener("touchmove", touchMove, false);
+content.addEventListener("touchstart", touchStart, false);
+content.addEventListener("touchmove", touchMove, false);
+setTimeout(function () {
+	wedding.addEventListener("touchend", touchEnd, false);
+},1000);
